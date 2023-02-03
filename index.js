b170a563259a47f663230364d4d2cc17044edd30
Vinicius de Morais <pvmoraiss@gmail.com>
2023-02-03 14:25:31 -0600

    one round of the gaming working

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("lalala")
+//Function that can make a computer do a random selection
+function getComputerChoice () {
+    let weapons = ["paper", "rock", "scissors"]
+    let useWeapon = weapons [Math.floor(Math.random() * weapons.length)]
+    return useWeapon
+}
+
+// Function that can compare player selection and computer selection
+function roundOne(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie!"
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper') ||
+    (playerSelection == 'paper' && computerSelection == 'rock')) {
+        return ('You win! ' + playerSelection + ' beats ' + computerSelection)
+    } else {
+        return ('Computer win! ' + computerSelection + ' beats ' + playerSelection)
+    }
+}
+
+//Calling both Functions and printing on console
+let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase()
+let computerSelection = getComputerChoice ()
+console.log("You choosed:" + playerSelection)
+console.log("Computer choosed:" + computerSelection)
+console.log(roundOne(playerSelection, computerSelection))
